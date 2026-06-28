ec91ed43e77810ee6d4b26fcc94aeb2c3a4a2f40
Carlos Federico  Gunther <carlosfedericogunther@MacBook-Pro-de-Carlos.local>
2026-06-28 03:34:10 -0300

    Fix session cookies - set secure=true for HTTPS

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production', // HTTPS en prod
+    secure: true,
     httpOnly: true,
     sameSite: 'lax'
   }
